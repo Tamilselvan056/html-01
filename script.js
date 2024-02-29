b77231d7d34a862aa41503122de831c71415ac34
Tamilselvan P <141137556+Tamilselvan056@users.noreply.github.com>
2024-02-29 09:52:43 +0530

    html code for  simple portfolio template

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+ //document.write("WELCOME TO MY SITE")
+ 
+ function display(){
+    
+    //var element = document.getElementById("para1")
+    //console.log(element)
+    var element = document.querySelector("para1")
+    element.i
+
+
+ }
+ display()
